294e1e2b2e23a434ffae50747bf0cba40610a1da
dhana <dhanasekharvk@gmail.com>
2023-09-14 21:06:21 +0530

    v1

--- code_check_1.py
+++ code_check_1.py
@@ -2,9 +2,9 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-def scrappy_fun(array):
-    """Function used to retrieve the chain name : uniport for a given PDB id
-    :array: will take the list of array of pdb_list
+def scrappy_fun(array: list) -> dict:
+    """Function used to retrieve the chain name : uniport for a given PDB id.
+    :array: will take the list of array of pdb_list.
     """
     dic_1 ={}
     patern = re.compile(r'\w+')
@@ -14,14 +14,13 @@ def scrappy_fun(array):
         pageurls = r"https://www.rcsb.org/structure/"+r[:4]
         try:
             url = requests.get(pageurls)
-
         except requests.exceptions.RequestException as e:
             print("Connection ERROR")
             break
-
         if url.status_code==200:   
             soup = BeautifulSoup(url.content, 'html.parser')
             chain_dic = {}
+
             for i in range(1,5):
                 need= soup.find_all('tr',{'id':f"macromolecule-entityId-{i}-rowDescription"})
                 if len(need)!=0:
@@ -32,15 +31,13 @@ def scrappy_fun(array):
                         uniport_id= uniport_2[0].text
                     else:
                         uniport_id=None
-                    print(chain,uniport_id)
                     chain_dic[chain] =uniport_id
                     dic_1[r.lower()] =chain_dic
                 else:
-                    pass
+                    pass  
         else:
             print(url.status_code, "Error")
             pass
-        
     return dic_1
 
 
--- test.py
+++ test.py
@@ -1,5 +1,76 @@
+import requests
+import re
+import multiprocessing
+from bs4 import BeautifulSoup
 
-path_to_file = r"C:\Users\dhana\OneDrive\Desktop\thesis_work_final\prj501\Single_mutation\run_script\uniport_sadh\data\maindata.txt"
-with open(path_to_file,'r') as file:
-    test_list= file.readlines()
-# print(test_list)
+def chain_crawl(html,x):
+    """Function used for looping the chain tables in the PDB website.
+    And retrieve the chain and uniport id of the chain
+    :html: the html tags from beautifulsoup
+    :x: int indicates chain column class in the pdb page
+    :y: dictionary for getting value"""
+
+    chain_dic = {}
+    need = html.find_all('tr',{'id':f"macromolecule-entityId-{x}-rowDescription"})
+    if len(need)!=0:
+        chain = need[0].find('td',{'style':'width:200px;'}).text 
+        uniport_1 = html.find_all("table", attrs={"id":f"table_macromolecule-protein-entityId-{x}"})
+        uniport_2 = uniport_1[0].find_all(href=re.compile("https://www.uniprot.org/uniprot"))
+        if len(uniport_2)!=0:
+            uniport_id = uniport_2[0].text
+        else:
+            uniport_id = None
+        print(chain,uniport_id)
+        chain_dic[chain] = uniport_id
+        return chain_dic
+    else:
+        return None
+    
+def sq(x,y):
+    return x**y
+
+if __name__ ==  "__main__":
+
+        pageurls = r"https://www.rcsb.org/structure/"+"1mky"
+        try:
+            url = requests.get(pageurls)
+        except requests.exceptions.RequestException as e:
+            print("Connection ERROR")
+
+        if url.status_code==200:   
+            soup = BeautifulSoup(url.content, 'html.parser')
+            const_v= soup
+            with multiprocessing.Pool(processes=2) as pool:
+                result = pool.starmap(chain_crawl, [(const_v, args) for args in [1,2,3,4,5]])
+                # result = pool.starmap(sq, [(3, args) for args in [1,2,3,4,5]])
+            print(result)
+        else:
+            print(url.status_code, "Error")
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
